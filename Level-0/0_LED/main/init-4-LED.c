8e6ac180542120e55a12231b171798aa6b0f445b
Yaoxin Ai <63080951+Shrimpaste@users.noreply.github.com>
2026-03-31 23:50:14 +0800

    Update init-4-LED.c

--- Level-0/0_LED/main/init-4-LED.c
+++ Level-0/0_LED/main/init-4-LED.c
@@ -2,10 +2,17 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#define LED_GPIO 38
 
 void ledinit(void)
 {
-    gpio_reset_pin(LED_GPIO);
-    gpio_set_direction(LED_GPIO, GPIO_DIRECTION_OUT);
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_GPIO),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf);
     gpio_set_level(LED_GPIO, 0);
 }
